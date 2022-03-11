912e6e104f6d6145d2210f7a2b8a39a45c98c6fe
Khrir <karllisson.15henrique@hotmail.com>
2022-03-10 22:32:55 -0300

    rm

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
-// const cors = require("cors");
 
 const routes = require("./routes/routes");
 const connectBD = require("./database/banco")
@@ -12,19 +11,6 @@ connectBD();
 const path = require('path');
 const app = express();
 const port = 3001;
-
-// app.use(cors);
-
-const session = require('express-session');
-app.use(session({
-    name : 'codeil',
-    secret : 'something',
-    resave :false,
-    saveUninitialized: true,
-    cookie : {
-            maxAge:(1000 * 60 * 100)
-    }
-}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded ({ extended: false}));
